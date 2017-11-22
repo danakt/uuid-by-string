97346e000688939c3a06ff55907773ee20aa4e09
Danakt Frost <mail@danakt.ru>
2017-11-22 23:20:09 +0300

    changes indentation to 2 spaces

--- src/index.js
+++ src/index.js
@@ -19,39 +19,39 @@ var DEFAULT_UUID = '00000000-0000-4000-8000-000000000000'
  * @return {string}
  */
 function generatePart(input, key, maxlen) {
-    // 14-digit number in hex is 16-digit in base-10, in turn, the js
-    // rounds everything that comes after the 16th sign among
-    if (maxlen == null || maxlen > 14) {
-        return generatePart(input, key, 14)
+  // 14-digit number in hex is 16-digit in base-10, in turn, the js
+  // rounds everything that comes after the 16th sign among
+  if (maxlen == null || maxlen > 14) {
+    return generatePart(input, key, 14)
+  }
+
+  var n = 1
+  var i = 1
+  var count = 1
+  var str = input.trim()
+
+  while (true) {
+    if (count >= str.length && n.toString(16).length >= maxlen) {
+      break
     }
 
-    var n = 1
-    var i = 1
-    var count = 1
-    var str = input.trim()
+    count++
 
-    while (true) {
-        if (count >= str.length && n.toString(16).length >= maxlen) {
-            break
-        }
-
-        count++
-
-        if (str[i] == null) {
-            i = 0
-        }
-
-        n *= (str.charCodeAt(i) + (i * str.length)) * key
-        n = removeTrailingZeros(n)
-
-        while (n.toString(16).length > maxlen) {
-            n = Math.floor(n / 10)
-        }
-
-        i++
+    if (str[i] == null) {
+      i = 0
     }
 
-    return n.toString(16)
+    n *= (str.charCodeAt(i) + (i * str.length)) * key
+    n = removeTrailingZeros(n)
+
+    while (n.toString(16).length > maxlen) {
+      n = Math.floor(n / 10)
+    }
+
+    i++
+  }
+
+  return n.toString(16)
 }
 
 /**
@@ -60,19 +60,19 @@ function generatePart(input, key, maxlen) {
  * @return {string}
  */
 function makeUUID(parts) {
-    var init = parts[0]
-    var mid  = parts[1]
-    var fin  = parts[2]
+  var init = parts[0]
+  var mid  = parts[1]
+  var fin  = parts[2]
 
-    var s = [
-        init,
-        mid.substr(0, 4),
-        4 + mid.substr(4, 3),
-        (Number('0x' + mid[7]) & 0x3 | 0x8).toString(16) + mid.substr(8, 3),
-        fin
-    ]
+  var s = [
+    init,
+    mid.substr(0, 4),
+    4 + mid.substr(4, 3),
+    (Number('0x' + mid[7]) & 0x3 | 0x8).toString(16) + mid.substr(8, 3),
+    fin
+  ]
 
-    return s.join('-').toUpperCase()
+  return s.join('-').toUpperCase()
 }
 
 /**
@@ -81,21 +81,21 @@ function makeUUID(parts) {
  * @return {string}     — UUID
  */
 function getUUIDByString(str) {
-    if (str.length === 0) {
-        return DEFAULT_UUID
-    }
+  if (str.length === 0) {
+    return DEFAULT_UUID
+  }
 
-    var keysTable = [
-        [0xf6, 8],
-        [0x51c, 11],
-        [0xd7a, 12],
-    ]
+  var keysTable = [
+    [0xf6, 8],
+    [0x51c, 11],
+    [0xd7a, 12],
+  ]
 
-    var uuidParts = keysTable.map(function(item) {
-        return generatePart(str, item[0], item[1])
-    })
+  var uuidParts = keysTable.map(function (item) {
+    return generatePart(str, item[0], item[1])
+  })
 
-    return makeUUID(uuidParts)
+  return makeUUID(uuidParts)
 }
 
 /**
@@ -104,22 +104,22 @@ function getUUIDByString(str) {
  * @return {number}
  */
 function removeTrailingZeros(int) {
-    var out = int
+  var out = int
 
-    while(Math.round(out / 10) * 10 === out) {
-        out /= 10
-    }
+  while (Math.round(out / 10) * 10 === out) {
+    out /= 10
+  }
 
-    return out
+  return out
 }
 
 /**
  * @exports
  */
 if (typeof module !== 'undefined' && module.exports) {
-    module.exports = getUUIDByString
+  module.exports = getUUIDByString
 } else if (typeof window !== 'undefined') {
-    window.getUUID = getUUIDByString
+  window.getUUID = getUUIDByString
 } else {
-    throw new Error('Unknown environment')
+  throw new Error('Unknown environment')
 }
